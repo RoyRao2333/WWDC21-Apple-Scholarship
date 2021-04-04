323ab51511433d63a41be0c86e8c829e0e84b741
RoyRao2333 <royrao2333@gmail.com>
2021-04-04 15:05:07 +0800

    no message

---
+++ How to comprehend the theory of relativity?.playground/Pages/2. A great step.xcplaygroundpage/Contents.swift
@@ -0,0 +1,50 @@
+//: ## ☪︎ Take a great step of humankind
+/*:
+ ___
+- Important:
+This is the second chapter of my submission. Be sure you've already explored the first chapter! :) \
+In this chapter, you'll learn some interesting things about the beautiful planet __Mars__. Let's get down to our business!
+ ___
+*/
+//: ### Preface
+/*:
+- callout(Mars, what are you?):
+Mars, the fourth closest planet to the sun and the second smallest planet in the solar system after Mercury, is one of the four terrestrial planets in the solar system. Mars is the god of war in Roman mythology, and in English we often called it _the red planet_. In ancient Chinese, it was called "phosphor" because it was like fire, and its position and brightness often changed so confusingly that people could not predict it. \
+Mars is basically a desert planet, with sand dunes, gravel and no stable liquid water. Like Earth, Mars has a diverse landscape of mountains, plains and canyons. Mars has a relatively thin atmosphere, with an average surface pressure of only 6 hPa. That's about 0.6 percent of Earth's surface pressure, just as 35 kilometers above Earth's surface. The air pressure is so low that sound can travel only about 1.5 percent of the distance compared to Earth.
+*/
+/*:
+- callout(Great achievements in history):
+Mars has been explored by dozens of unmanned spacecraft. Launched by NASA on November 28, 1964, _Mariner 4_ was the first spacecraft to visit Mars and made its closest approach to the planet on July 15, 1965. The Soviet spacecraft _Mars 3_ made a soft landing in December 1971, but lost contact within seconds. The European Space Agency (ESA) became the third space agency to send a probe to Mars, with its _Mars Express_, which reached Mars' orbitå on December 25, 2003. On September 24, 2014, _Mangalyaan_ space probe of the Indian Space Research Organisation (ISRO) reached Mars' orbit, and ISRO became the fourth space agency to successfully send a probe to Mars. Then there are the United Arab Emirates' _Hope_ and China's _Tianwen-1_.
+*/
+/*:
+- callout(A milestone for China’s move toward deep space):
+_Tianwen-1_, China's Mars probe which is researched and developed autonomously by China National Space Administration (CNSA), was launched at 12:41 on July 23, 2020 by _CZ-5_ rocket from Wenchang Spacecraft Launch Site and successfully sent into a predetermined orbit. _Tianwen-1_'s stated goals are to look for evidence of current and past life and to assess the planet's environment. The probe arrived near Mars after about seven months of transit, and then entered orbit on February 10, 2021. For the next 2 months after that, the space probe will study the target site from a reconnaissance orbit, then in May or June 2021 the landing is planned to begin with the release of the capsule. \
+The word _Tianwen_ is derived from the poem _Heavenly Questions_ written by Qu Yuan. It's a piece contained in the classical Chinese poetry collection _Chu Ci_. In _Heavenly Questions_, Qu Yuan boldly raised his doubt to some traditional ideas and showed his spirit of exploration in pursuit of truth. Like Qu Yuan, _Tianwen-1_ expresses the perseverance and persistence of the humanity in the pursuit of truth, and implies the long journey to explore scientific truth and the endless pursuit of technological innovation.
+*/
+/*:
+![Southern_Hemisphere](Preface%20Assets/southern_hemisphere.jpeg "The southern hemisphere image was captured by Tianwen-1 on March 16, 2021, when the probe was orbiting at an altitude of about 11,200 kilometers.")
+![Northern_Hemisphere](Preface%20Assets/northern_hemisphere.jpeg "The northern hemisphere image was captured by Tianwen-1 on March 18, 2021, when the probe was orbiting at an altitude of about 11,500 kilometers.")
+ 
+&nbsp;
+ 
+&nbsp;
+ 
+*/
+//: ### You May Need To Know
+/*:
+- callout(Hohmann transfer orbit:):
+ In 1925, German aerospace engineer Dr. Walter Hormann published the book _Die Erreichbarkeit der Himmelskörper_, in which he proposed the famous _Hormann transfer orbit_. Hohmann transfer orbit is an elliptical orbit used to transfer between two circular orbits of different radii around a central body in the same plane. The Hohmann transfer often uses the lowest possible amount of propellant in traveling between these orbits.
+*/
+/*:
+- Experiment:
+The little game in this chapter provides you a clear view of how a space probe leave Earth and successfully land on Mars. \
+Already itching to try? Run the code and find it out!
+*/
+
+import Cocoa
+import SwiftUI
+import PlaygroundSupport
+
+var str = "Hello, playground"
+
+//: [Previous Chapter](1.%20Every%20child%20has%20a%20space%20dream!)
