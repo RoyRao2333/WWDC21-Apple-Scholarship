4ea7f2a46398b1e572990bb918a3fc12e4a541d3
RoyRao2333 <royrao2333@gmail.com>
2021-04-13 21:43:44 +0800

    no message

--- How to comprehend the theory of relativity?.playground/Pages/Length Contraction & Time Dilation.xcplaygroundpage/Contents.swift
+++ How to comprehend the theory of relativity?.playground/Pages/Length Contraction & Time Dilation.xcplaygroundpage/Contents.swift
@@ -1,4 +1,4 @@
-//: ## ☪︎ Length Contraction and Time Dilation
+//: ## ☪︎ Time Dilation and Length Contraction
 /*:
  ___
 - Important:
@@ -8,57 +8,6 @@ In this chapter, you can learn two classic phenomena of special relativity. Go a
 */
 /*:
 
-&nbsp;
- 
-### Length Contraction
-*/
-/*:
-- callout(Basic concept):
-Length contraction refers to the phenomenon that the length becomes smaller when the observer looks at an object with a non-zero relative velocity. This phenomenon is usually more obvious only when the relative velocity is close to the speed of light. And the length contraction can only be observed in the direction parallel to the motion of the object. For common objects, this effect can be ignored when they move at a normal speed. It makes sense to consider this phenomenon only if the speed of its motion is large enough, or in the motion of electrons.
- 
-&nbsp;
- 
-As the speed approaches the speed of light, this effect begins to become very obvious. \
-This effect can be described by the following equation: \
-![LC Equation](Preface_Assets/Chapter_I/LC_Equation.png)
- 
-- _L0_ is the inherent length of the object (the length of the object in the frame of reference relative to it)
-- _L_ is the length of the object observed by the observer
-- _v_ is the relative velocity between the observer and the moving object
-- _c_ is the speed of light
- 
-And _γ(v)_ / _γ_ is the Lorentz factor, defined as: \
-![Lorentz factor](Preface_Assets/Chapter_I/Lorentz_factor.png)
- 
-&nbsp;
- 
-In this equation, the length of the object is parallel to the direction of its motion. The observer needs to measure the distance from both ends of the object at the same time and then subtract to get the length of the object. \
-When the relative velocity approaches the speed of light, the length of the object in the corresponding direction becomes zero.
- 
-![DIVIDER](Preface_Assets/divider.png)
- 
-- callout(Spacetime diagram):
-__Spacetime diagram__, also known as __Minkowski diagrams__, was developed by _Hermann Minkowski_ in 1908. It is a graphical illustration of the properties of space and time in the special theory of relativity, and used to represent the coordinates of events in Minkowski spacetime. It is a tool for understanding the phenomena of special relativity.
- 
-&nbsp;
- 
-In the __four-dimensional coordinate system__, time times the speed of light (ct) as one of the axes, called the time axis; the other x-axis, y-axis, z-axis are called the space axis. Every point in this four-dimensional spacetime represents an event __E__. Corresponding to a specific inertial reference frame, the time and place where __E__ occurs can be represented as __(ct, x, y, z)__. \
-The activity of each particle in spacetime can be represented by a continuous curve on the spacetime diagram, which is called __the world line__.
- 
-&nbsp;
- 
-- Note: In order to facilitate the representation on the plane, some of the Minkowski diagrams we normally see only have a time axis (ct) and one space axis (x).
-*/
-/*:
- 
-___
-
-&nbsp;
- 
-&nbsp;
- 
-&nbsp;
- 
 &nbsp;
  
 ### Time Dilation
@@ -83,12 +32,13 @@ __Gravitational time dilation__, on the other hand, is not reciprocal. Observers
 The equation for measuring time dilation in the special theory of relativity is: \
 ![TD_Equation](Preface_Assets/Chapter_I/TD_Equation.png)
  
+where
 - _Δt_ is the time interval between two local events (that is, two events that occur in the same place) according to the clock of an observer. This is called __proper time__.
 - _Δt'_ is the time interval between the same two events according to the clock of another observer.
 - _v_ is the velocity at which the second clock moves relative to the first clock.
 - _c_ is the speed of light.
  
-And _γ_ is still the Lorentz factor, defined as: \
+And _γ_ is the __Lorentz factor__, defined as: \
 ![Lorentz factor](Preface_Assets/Chapter_I/Lorentz_factor.png)
  
 &nbsp;
@@ -99,11 +49,63 @@ In daily life, even the time dilation effect caused by high-speed spacecraft is 
 /*:
  
 ___
+
+&nbsp;
+ 
+&nbsp;
+ 
+&nbsp;
+ 
+&nbsp;
+ 
+### Length Contraction
+*/
+/*:
+- callout(Basic concept):
+Length contraction refers to the phenomenon that the length becomes smaller when the observer looks at an object with a non-zero relative velocity. This phenomenon is usually more obvious only when the relative velocity is close to the speed of light. And the length contraction can only be observed in the direction parallel to the motion of the object. For common objects, this effect can be ignored when they move at a normal speed. It makes sense to consider this phenomenon only if the speed of its motion is large enough, or in the motion of electrons.
+ 
+&nbsp;
+ 
+As the speed approaches the speed of light, this effect begins to become very obvious. \
+This effect can be described by the following equation: \
+![LC Equation](Preface_Assets/Chapter_I/LC_Equation.png)
+ 
+where
+- _L0_ is the inherent length of the object (the length of the object in the frame of reference relative to it)
+- _L_ is the length of the object observed by the observer
+- _v_ is the relative velocity between the observer and the moving object
+- _c_ is the speed of light
+ 
+And _γ(v)_ / _γ_ is still the __Lorentz factor__, defined as: \
+![Lorentz factor](Preface_Assets/Chapter_I/Lorentz_factor.png)
+ 
+&nbsp;
+ 
+In this equation, the length of the object is parallel to the direction of its motion. The observer needs to measure the distance from both ends of the object at the same time and then subtract to get the length of the object. \
+When the relative velocity approaches the speed of light, the length of the object in the corresponding direction becomes zero.
+ 
+![DIVIDER](Preface_Assets/divider.png)
+ 
+- callout(Spacetime diagram):
+__Spacetime diagram__, also known as __Minkowski diagrams__, was developed by _Hermann Minkowski_ in 1908. It is a graphical illustration of the properties of space and time in the special theory of relativity, and used to represent the coordinates of events in Minkowski spacetime. It is a tool for understanding the phenomena of special relativity.
+ 
+&nbsp;
+ 
+In the __four-dimensional coordinate system__, time times the speed of light (ct) as one of the axes, called the time axis; the other x-axis, y-axis, z-axis are called the space axis. Every point in this four-dimensional spacetime represents an event __E__. Corresponding to a specific inertial reference frame, the time and place where __E__ occurs can be represented as __(ct, x, y, z)__. \
+The activity of each particle in spacetime can be represented by a continuous curve on the spacetime diagram, which is called __the world line__.
+ 
+&nbsp;
+ 
+- Note: In order to facilitate the representation on the plane, some of the Minkowski diagrams we normally see only have a time axis (ct) and one space axis (x).
+*/
+/*:
+ 
+___
  
 &nbsp;
  
 - Experiment:
-In the mini game in this chapter, you can understand at a glance what __length contraction__ and __time dilation__ are, and you can also have a certain understanding of the special theory of relativity. \
+In the mini game in this chapter, you can understand at a glance what __time dilation__ and __length contraction__ are, and you can also have a certain understanding of the special theory of relativity. \
 Already itching to try? Run the code and find it out yourself!
 */
 
