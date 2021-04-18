dffeb5495e3e507291467089d09f0ee6f34a064b
RoyRao2333 <royrao2333@gmail.com>
2021-04-18 23:58:01 +0800

    no message

--- A simple understanding of the theory of relativity.playground/Pages/Interstellar travel and more.xcplaygroundpage/Contents.swift
+++ A simple understanding of the theory of relativity.playground/Pages/Interstellar travel and more.xcplaygroundpage/Contents.swift
@@ -3,7 +3,7 @@
  ___
 - Important:
  __Please adjust the game window to a suitable size while playing!__ (Safe area is marked with green borders) \
-This is the _third chapter_ of my Playground. Be sure you've finished the previous two chapters. If so, then please go ahead and enjoy this chapter! :) \
+This is the _second chapter_ of my Playground. Be sure you've finished the previous two chapters. If so, then please go ahead and enjoy this chapter! :) \
 In this chapter, you can learn something interesting about __interstellar travel__ and some thought-provoking experiments, such as the famous __Twin Paradox__. Now go ahead!
  ___
 */
